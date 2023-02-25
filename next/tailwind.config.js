2370869b2bfd6ef139585058049f9cff7e61b2d4
Francis Gregori Munis <contato@francisgregori.com.br>
2023-02-25 20:26:55 -0300

    refactor: add sandy color to tailwind theme

--- next/tailwind.config.js
+++ next/tailwind.config.js
@@ -8,6 +8,20 @@ module.exports = {
     ],
     theme: {
         extend: {
+            colors: {
+                sandy: {
+                    '50': '#fef7ee',
+                    '100': '#fcedd8',
+                    '200': '#f8d8b0',
+                    '300': '#f4bb7d',
+                    '400': '#ee964b',
+                    '500': '#e97826',
+                    '600': '#db5f1b',
+                    '700': '#b54819',
+                    '800': '#913a1b',
+                    '900': '#753219',
+                }
+            },
             fontFamily: {
                 sans: ['Inter var', ...defaultTheme.fontFamily.sans],
             },
