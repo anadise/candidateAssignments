6009cea0384cdbaa8c310094700bc0357a7ac7f8
Karan Chauhan <karanmilanbhai.chauhan@dcmail.ca>
2023-02-27 13:57:03 -0500

    Implemented function to request client data.

--- next/src/pages/index.tsx
+++ next/src/pages/index.tsx
@@ -2,8 +2,29 @@ import type { NextPage } from 'next';
 import { useState, useEffect } from 'react';
 import { request } from '../utils/frontEnd';
 import ClientTable from '../components/tables/client';
+import { ClientData } from "../types";
 
 const Index: NextPage = () => {
+    //initializing state
+    const [clients, setClients] = useState<ClientData[]>([])
+
+    useEffect(() => {
+        //Getting Data
+        try {
+            request('GET', '/clients').then(({ body: data, status }) => {
+                if (status === 200) {
+                    setClients(data?.clients.map((client: ClientData) => ({
+                        ...client,
+                        fullName: `${client.firstName} ${client.lastName}`
+                    })))
+                }
+            })
+        } catch (err) {
+        }
+    }, [])
+    //Registering Data
+    //TODO: Register function to register new client
+
     return (
         <>
             <ClientTable clients={clients} onRegister={onRegister} />
