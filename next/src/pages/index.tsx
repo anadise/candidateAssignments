29d8a9c0517fde70bc6af39697b2c0944be2f064
Andrus <xwen0110@gmail.com>
2023-03-13 16:52:20 +0200

    feat: Implement fetching clients

--- next/src/pages/index.tsx
+++ next/src/pages/index.tsx
@@ -2,8 +2,24 @@ import type { NextPage } from 'next';
 import { useState, useEffect } from 'react';
 import { request } from '../utils/frontEnd';
 import ClientTable from '../components/tables/client';
+import { IClient } from '../types/client';
 
 const Index: NextPage = () => {
+    const [clients, setClients] = useState<IClient[]>([])
+    
+    useEffect(() => {
+        const fetchClients = async () => {
+            try {
+                const data = await request("GET", "/clients")
+                setClients(data.body)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        fetchClients()
+    }, [])
+
     return (
         <>
             <ClientTable clients={clients} onRegister={onRegister} />
---
+++ next/src/types/client.ts
@@ -0,0 +1,8 @@
+export interface IClient {
+  id: string;
+  avatar: string;
+  email: string;
+  fullName: string;
+  supportTier: 'standard' | 'gold' | 'platinum';
+  hourlyRate: number;
+}
