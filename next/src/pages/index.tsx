a8115aa14cb99f1d33f776deb80787749c03fc92
Akshil Thumar <akshilvthumar@gmail.com>
2023-03-15 10:34:50 +0530

    Tasks completed: 1, 2, 3

--- next/src/pages/index.tsx
+++ next/src/pages/index.tsx
@@ -2,13 +2,36 @@ import type { NextPage } from 'next';
 import { useState, useEffect } from 'react';
 import { request } from '../utils/frontEnd';
 import ClientTable from '../components/tables/client';
+import clients from './api/clients';
+
+type clients_type = {
+  id: string;
+  avatar: string;
+  email: string;
+  fullName: string;
+  supportTier: 'standard' | 'gold' | 'platinum';
+  hourlyRate: number;
+}[];
 
 const Index: NextPage = () => {
-    return (
-        <>
-            <ClientTable clients={clients} onRegister={onRegister} />
-        </>
-    );
+  function onRegister(): void {
+    throw new Error('Function not implemented.');
+  }
+  const [clients, setClients] = useState<clients_type>([]);
+
+  // ------------------ Fetching data from API ------------------
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch('/api/clients').then((res) => res.json());
+      setClients(result.clients);
+    };
+    fetchData();
+  }, []);
+  // ------------------ Fetching data from API ------------------
+
+  return (
+    <>{clients && <ClientTable clients={clients} onRegister={onRegister} />}</>
+  );
 };
 
 export default Index;
