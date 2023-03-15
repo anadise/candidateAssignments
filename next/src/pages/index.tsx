2b8e2e3a7b4962de1d1b56c0ee2a626793962b15
zhang <zangfang.111@gmail.com>
2023-03-15 17:46:01 +0800

    chore: added lint & prettierrc && lint issues fix

--- next/src/pages/index.tsx
+++ next/src/pages/index.tsx
@@ -4,41 +4,41 @@ import { request } from '../utils/frontEnd';
 import ClientTable from '../components/tables/client';
 
 export type ClientsTypes = {
-    id: string;
-    avatar: string;
-    birthday: Date;
-    email: string;
-    firstName: string;
-    lastName: string;
-    sex: string;
-    supportTier: 'standard' | 'gold' | 'platinum';
-    hourlyRate: number;
-}
+  id: string;
+  avatar: string;
+  birthday: Date;
+  email: string;
+  firstName: string;
+  lastName: string;
+  sex: string;
+  supportTier: 'standard' | 'gold' | 'platinum';
+  hourlyRate: number;
+};
 
 interface IndexPageProps {
-    clients: ClientsTypes[];
+  clients: ClientsTypes[];
 }
 
 const Index: React.FC<IndexPageProps> = ({ clients }) => {
-    const onRegister = () => {
-        console.log('onRegister');
-    };
+  const onRegister = () => {
+    console.log('onRegister');
+  };
 
-    return (
-        <>
-            <ClientTable clients={clients} onRegister={onRegister} />
-        </>
-    );
+  return (
+    <>
+      <ClientTable clients={clients} onRegister={onRegister} />
+    </>
+  );
 };
 
 export default Index;
 
 export const getServerSideProps = async () => {
-    // get clients with request function
-    const res = await request('GET', '/clients');
+  // get clients with request function
+  const res = await request('GET', '/clients');
 
-    // validation: if clients is undefined or null, return notfound.
-    if (!res?.body) return { notFound: true }
+  // validation: if clients is undefined or null, return notfound.
+  if (!res?.body) return { notFound: true };
 
-    return { props: { clients: res.body.clients } };
+  return { props: { clients: res.body.clients } };
 };
