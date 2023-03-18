5098204b47583b5eb2f22090dd6a490262eaebd9
AnouKhamtan0726 <talentboy726@gmail.com>
2023-03-18 09:44:54 +0800

    feat: displayed full name

--- next/src/pages/index.tsx
+++ next/src/pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next';
 import { useState, useEffect } from 'react';
-import axios from 'axios';
 import { request } from '../utils/frontEnd';
 import ClientTable from '../components/tables/client';
 
@@ -8,8 +7,17 @@ const Index: NextPage = () => {
     const [clients, setClients] = useState([])
 
     useEffect(() => {
-        axios.get('/api/clients').then((res) => {
-            setClients(res.data.clients)
+        request("get", "/clients").then((res) => {
+            if (res.status == 200) {
+                console.log(res.body.clients)
+                res.body.clients.map((e) => {
+                    e.fullName = e.firstName + " " + e.lastName
+
+                    return e
+                })
+
+                setClients(res.body.clients)
+            }
         })
     }, [])
 
