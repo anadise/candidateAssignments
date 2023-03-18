e18f3e9979c544cfc449c85c1ff3fe86d1d9290d
AnouKhamtan0726 <talentboy726@gmail.com>
2023-03-18 09:32:57 +0800

    feat: called /api/clients to show clients table

--- next/src/pages/index.tsx
+++ next/src/pages/index.tsx
@@ -1,9 +1,22 @@
 import type { NextPage } from 'next';
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 import { request } from '../utils/frontEnd';
 import ClientTable from '../components/tables/client';
 
 const Index: NextPage = () => {
+    const [clients, setClients] = useState([])
+
+    useEffect(() => {
+        axios.get('/api/clients').then((res) => {
+            setClients(res.data.clients)
+        })
+    }, [])
+
+    function onRegister() {
+
+    }
+
     return (
         <>
             <ClientTable clients={clients} onRegister={onRegister} />
