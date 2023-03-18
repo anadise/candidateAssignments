894567aa2f4460300d132b2998c29bda06ffb45e
AnouKhamtan0726 <talentboy726@gmail.com>
2023-03-18 10:06:13 +0800

    feat: get query parameter named highlight

--- next/src/pages/index.tsx
+++ next/src/pages/index.tsx
@@ -1,15 +1,18 @@
 import type { NextPage } from 'next';
+import { useRouter } from 'next/router'
 import { useState, useEffect } from 'react';
 import { request } from '../utils/frontEnd';
 import ClientTable from '../components/tables/client';
+import { useSearchParams } from "react-router-dom";
 
 const Index: NextPage = () => {
     const [clients, setClients] = useState([])
+    const [activeClient, setActiveClient] = useState("")
+    const router = useRouter()
 
     useEffect(() => {
         request("get", "/clients").then((res) => {
             if (res.status == 200) {
-                console.log(res.body.clients)
                 res.body.clients.map((e) => {
                     e.fullName = e.firstName + " " + e.lastName
 
@@ -21,12 +24,21 @@ const Index: NextPage = () => {
         })
     }, [])
 
+    useEffect(() => {
+        const { highlight } = router.query
+
+        console.log(highlight ?? "")
+
+        setActiveClient(highlight ?? "")
+    }, [router.query])
+
     function onRegister() {
 
     }
 
     return (
         <>
+            { activeClient }
             <ClientTable clients={clients} onRegister={onRegister} />
         </>
     );
