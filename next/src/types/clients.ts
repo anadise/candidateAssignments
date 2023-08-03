627f0031f4a3381daab6eeffcb1dcb4d2fe0d74f
Italo Nabuco <italonabuco@hotmail.com>
2023-08-03 17:47:48 -0300

    improving `IClient` types

--- next/src/types/clients.ts
+++ next/src/types/clients.ts
@@ -1,8 +1,19 @@
 export interface IClient {
-  id: string;
-  avatar: string;
-  email: string;
-  fullName: string;
-  supportTier: 'standard' | 'gold' | 'platinum';
-  hourlyRate: number;
+    id: string;
+    avatar: string;
+    email: string;
+    fullName: string;
+    supportTier: string;
+    hourlyRate: number;
+}
+
+export interface IClientRaw {
+    id: string;
+    avatar: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    sex: string;
+    supportTier: string;
+    hourlyRate: number;
 }
